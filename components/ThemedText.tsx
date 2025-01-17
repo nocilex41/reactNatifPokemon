0a09c930e0a2e0e7f4989e7eb3df2dd11eda3900
alexis vieira <alexisvieira@Mac.lan>
2025-01-17 23:35:47 +0100

    first commit

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,39 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
-
-import { useThemeColor } from '@/hooks/useThemeColor';
-
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
-
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
-
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
-}
+import { StyleSheet, Text } from "react-native";
+import type { TextProps } from "react-native";
 
 const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
+    body3: {
+        fontSize: 10,
+        lineHeight: 16,
+    },
+    headline: {
+        fontSize: 24,
+        lineHeight: 16,
+    },
+    caption: {
+        fontSize: 8,
+        lineHeight: 12,
+    },
+    subtitle1: {
+        fontSize: 14,
+        lineHeight: 16,
+        fontWeight: "bold",
+    },
+    subtitle3: {
+        fontSize: 10,
+        lineHeight: 16,
+        fontWeight: "bold",
+    },
 });
+
+type Props = TextProps & {
+        variant?: keyof typeof styles;
+        color?: string
+    }
+
+
+export function ThemedText ({variant, color, ...rest}: Props){
+    return <Text style={styles[variant ?? "body3"]} {...rest}/>
+
+}
+
